19addbd8f2fbadada63540e9f74a4caf04e39414
minacon <minacon655@gmail.com>
2019-07-01 02:46:04 +0900

    group作成・編集機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,15 @@
 class MessagesController < ApplicationController
-  def index
 
+  def index
+    @group = Group.find(message_params[:group_id])
   end
 
   def create
-    
   end
-  
+
+  private
+  def message_params
+    params.permit(:group_id)
+  end
+
 end
