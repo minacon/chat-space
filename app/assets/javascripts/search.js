0e09ab9993dd54eba3ec4d9c0f610e8796612b03
minacon <minacon655@gmail.com>
2019-07-11 10:43:12 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -28,29 +28,31 @@ $(document).on("turbolinks:load",function() {
 
   $('#user-search-field').on('keyup',function(e){
     e.preventDefault();
-    var input = $('#user-search-field').val();
 
-    $.ajax({
-      type: 'GET',
-      url: '/users/search',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    
-    .done(function(users) {
-      $('#user-search-result').empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          userHtml(user);
-        });
-      }
-      else {
+    var input = $('#user-search-field').val();
+    $('#user-search-result').empty();
+    if (input.length !== 0) {
+      $.ajax({
+        type: 'GET',
+        url: '/users/search',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+
+      .done(function(users) {
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            userHtml(user);
+          });
+        }
+        else {
+          errHtml("一致するuserはいません");
+        }
+      })
+      .fail(function() {
         errHtml("一致するuserはいません");
-      }
-    })
-    .fail(function() {
-      errHtml("一致するuserはいません");
-    });
+      });
+    }
   });
 
   $(document).on('click','.chat-group-user__btn--add',function(){
